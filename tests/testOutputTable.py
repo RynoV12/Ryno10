c58ec336f8505ae59dc921476ac87ab0012c8161
jfrancisohalloran <161669277+jfrancisohalloran@users.noreply.github.com>
2024-11-06 19:42:14 -0500

    reformatting

--- tests/testOutputTable.py
+++ tests/testOutputTable.py
@@ -20,28 +20,26 @@ for i in code:
             exec(compile("".join(i['source']), '<string>', 'exec'))
 
 
-# todo: replace this with an actual test
 class TestCase(unittest.TestCase):
 
     def testSummaryTableOutput(self):
-      stdout = system.stdout
-      system.stdout = io.StringIO()
+        stdout = system.stdout
+        system.stdout = io.StringIO()
 
+        data = pd.read_csv("tests/files/assignment8Data.csv")
+        x = data.loc[:100, ['sex','age','educ']]
+        y = data.loc[:100, 'white']
+        reg = RegressionModel(x, y, create_intercept=True, regression_type='logit')
+        reg.fit_model()
+        reg.summary()
 
-      data = pd.read_csv("tests/files/assignment8Data.csv")
-      x = data.loc[:100, ['sex','age','educ']]
-      y = data.loc[:100, 'white']
-      reg = RegressionModel(x, y, create_intercept=True, regression_type='logit')
-      reg.fit_model()
-      reg.summary()
+        output = system.stdout.getvalue()
+        system.stdout = stdout
 
-      output = system.stdout.getvalue()
-      system.stdout = stdout
+        labels = bool(re.findall(r'[Cc]oef.*[Ss]t.*[Zz].*[Pp].*', output))
+        sex = bool(re.findall(r'sex.*\d+.*\d+.*\d+.*\d+.*', output))
+        age = bool(re.findall(r'age.*\d+.*\d+.*\d+.*\d+.*', output))
+        educ = bool(re.findall(r'educ.*\d+.*\d+.*\d+.*\d+.*', output))
+        intercept = bool(re.findall(r'intercept.*\d+.*\d+.*\d+.*\d+.*', output))
 
-      labels = bool(re.findall(r'[Cc]oef.*[Ss]t.*[Zz].*[Pp].*', output))
-      sex = bool(re.findall(r'sex.*\d+.*\d+.*\d+.*\d+.*', output))
-      age = bool(re.findall(r'age.*\d+.*\d+.*\d+.*\d+.*', output))
-      educ = bool(re.findall(r'educ.*\d+.*\d+.*\d+.*\d+.*', output))
-      intercept = bool(re.findall(r'intercept.*\d+.*\d+.*\d+.*\d+.*', output))
-
-      self.assertTrue(labels&sex&age&educ&intercept, "Your table is not correctly formatted.")
+        self.assertTrue(labels & sex & age & educ & intercept, "Your table is not correctly formatted.")
